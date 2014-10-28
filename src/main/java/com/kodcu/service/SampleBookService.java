30e083c43e1e31be99ac23bffb57d72287f35b21
Rahman Usta <rahman.usta.88@gmail.com>
2014-10-28 09:42:43 +0200

    trying linguist

--- src/main/java/com/kodcu/service/SampleBookService.java
+++ src/main/java/com/kodcu/service/SampleBookService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
