02492da539c5e3ee23d4835a6d7e3f98b652643b
Rahman Usta <rahman.usta.88@gmail.com>
2014-12-22 01:15:32 +0200

    - Update icon getter

--- src/main/java/com/kodcu/service/AwesomeService.java
+++ src/main/java/com/kodcu/service/AwesomeService.java
@@ -20,13 +20,31 @@ public class AwesomeService {
     private PathResolverService pathResolver;
 
     public Node getIcon(Path path) {
-        AwesomeIcon awesomeIcon = null;
+        AwesomeIcon awesomeIcon = AwesomeIcon.FILE;
         if (Files.isDirectory(path))
             awesomeIcon = AwesomeIcon.FOLDER_ALT;
         else if (pathResolver.isAsciidoc(path))
             awesomeIcon = AwesomeIcon.FILE_TEXT_ALT;
         else if (pathResolver.isImage(path))
             awesomeIcon = AwesomeIcon.FILE_PICTURE_ALT;
+        else if (pathResolver.isPDF(path))
+            awesomeIcon = AwesomeIcon.FILE_PDF_ALT;
+        else if (pathResolver.isPPT(path))
+            awesomeIcon = AwesomeIcon.FILE_POWERPOINT_ALT;
+        else if (pathResolver.isDocx(path))
+            awesomeIcon = AwesomeIcon.FILE_WORD_ALT;
+        else if (pathResolver.isExcel(path))
+            awesomeIcon = AwesomeIcon.FILE_EXCEL_ALT;
+        else if (pathResolver.isArchive(path))
+            awesomeIcon = AwesomeIcon.ARCHIVE;
+        else if (pathResolver.isVideo(path))
+            awesomeIcon = AwesomeIcon.FILE_VIDEO_ALT;
+        else if (pathResolver.isHTML(path))
+            awesomeIcon = AwesomeIcon.HTML5;
+        else if (pathResolver.isCSS(path))
+            awesomeIcon = AwesomeIcon.CSS3;
+        else if (pathResolver.isBash(path))
+            awesomeIcon = AwesomeIcon.TERMINAL;
         Label iconLabel = AwesomeDude.createIconLabel(awesomeIcon, "14.0");
         return iconLabel;
     }
