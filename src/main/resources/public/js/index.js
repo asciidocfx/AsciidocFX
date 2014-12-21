68cef50781017b8233dfb62dff924e6d63c510e3
Rahman Usta <rahman.usta.88@gmail.com>
2014-12-22 01:14:57 +0200

    - look document language before using default language

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -2,64 +2,83 @@ var defaultLanguage = undefined;
 function getDefaultLanguage() {
     if (!defaultLanguage)
         defaultLanguage = app.getConfig().getDefaultLanguage();
-    return "lang=" + defaultLanguage;
+    return defaultLanguage;
 }
 
 function convertBasicHtml(content) {
+
     var options = Opal.hash2(['backend', 'safe', 'attributes'], {
         backend: 'html5',
         safe: 'safeMode',
-        attributes: 'showtitle icons=font@ source-highlighter=highlight.js platform=opal platform-opal env=browser env-browser ' + getDefaultLanguage()
+        attributes: 'showtitle icons=font@ source-highlighter=highlight.js platform=opal platform-opal env=browser env-browser '
     });
 
-    return Opal.Asciidoctor.$convert(content, options);
+    var doc = Opal.Asciidoctor.$load(content,options);
+
+    doc.attributes.keys["lang"] = doc.attributes.keys["lang"] || getDefaultLanguage();
+
+    return doc.$convert();
 }
 
 function convertHtmlBook(content) {
+
     var options = Opal.hash2(['backend', 'safe', 'attributes', "header_footer"], {
         backend: 'html5',
         safe: 'safeMode',
-        attributes: 'showtitle icons=font@ source-highlighter=highlight.js platform=opal platform-opal env=browser env-browser ' + getDefaultLanguage()
+        attributes: 'showtitle icons=font@ source-highlighter=highlight.js platform=opal platform-opal env=browser env-browser '
         , 'header_footer': true
     });
-    var rendered = Opal.Asciidoctor.$render(content, options);
 
-    return rendered;
+    var doc = Opal.Asciidoctor.$load(content,options);
+
+    doc.attributes.keys["lang"] = doc.attributes.keys["lang"] || getDefaultLanguage();
+
+    return doc.$render();
 }
 
 function convertHtmlArticle(content) {
-    var hash2 = Opal.hash2(['attributes', 'header_footer'],
+
+    var options = Opal.hash2(['attributes', 'header_footer'],
         {
-            'attributes': ['backend=html5', 'doctype=article', getDefaultLanguage()],
+            'attributes': ['backend=html5', 'doctype=article'],
             'header_footer': true
         });
-    var rendered = Opal.Asciidoctor.$render(content, hash2);
 
-    return rendered;
+    var doc = Opal.Asciidoctor.$load(content,options);
+
+    doc.attributes.keys["lang"] = doc.attributes.keys["lang"] || getDefaultLanguage();
+
+    return doc.$render();
 }
 
 function convertDocbook(content, includeHeader) {
 
-    var hash2 = Opal.hash2(['attributes', 'header_footer'],
+    var options = Opal.hash2(['attributes', 'header_footer'],
         {
-            'attributes': ['backend=docbook5', 'doctype=book', getDefaultLanguage()],
+            'attributes': ['backend=docbook5', 'doctype=book'],
             'header_footer': includeHeader
         });
-    var rendered = Opal.Asciidoctor.$render(content, hash2);
 
-    return rendered;
+    var doc = Opal.Asciidoctor.$load(content, options);
+
+    doc.attributes.keys["lang"] = doc.attributes.keys["lang"] || getDefaultLanguage();
+
+    return doc.$render();
 }
 
 function convertDocbookArticle(content) {
 
-    var hash2 = Opal.hash2(["lang", 'attributes', 'header_footer'],
+    var options = Opal.hash2(["lang", 'attributes', 'header_footer'],
         {
-            'attributes': ['backend=docbook5', 'doctype=article', getDefaultLanguage()],
+            'attributes': ['backend=docbook5', 'doctype=article'],
             'header_footer': true
         });
-    var rendered = Opal.Asciidoctor.$render(content, hash2);
 
-    return rendered;
+    var doc = Opal.Asciidoctor.$load(content,options);
+
+    doc.attributes.keys["lang"] = doc.attributes.keys["lang"] || getDefaultLanguage();
+
+    return doc.$render();
 }
 
 function scrollTo60(position) {
