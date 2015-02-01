c5161fc786675c6606d95a15d68891c08ee35a6b
Rahman Usta <rahman.usta.88@gmail.com>
2015-02-01 17:13:12 +0200

    PathOrder remove grouping functionality

--- src/main/java/com/kodcu/service/PathOrderService.java
+++ src/main/java/com/kodcu/service/PathOrderService.java
@@ -14,30 +14,7 @@ import java.util.function.Function;
 @Component
 public class PathOrderService {
 
-    @Autowired
-    private PathResolverService pathResolver;
-
     public int comparePaths(Path first, Path second) {
-
-        if (comparePathsFunction(first, second, pathResolver::isBook)) return 1;
-        if (comparePathsFunction(second, first, pathResolver::isBook)) return -1;
-        if (comparePathsFunction(first, second, pathResolver::isAsciidoc)) return 1;
-        if (comparePathsFunction(second, first, pathResolver::isAsciidoc)) return -1;
-        if (comparePathsFunction(first, second, pathResolver::isPDF)) return 1;
-        if (comparePathsFunction(second, first, pathResolver::isPDF)) return -1;
-        if (comparePathsFunction(first, second, pathResolver::isHTML)) return 1;
-        if (comparePathsFunction(second, first, pathResolver::isHTML)) return -1;
-        if (comparePathsFunction(first, second, pathResolver::isEpub)) return 1;
-        if (comparePathsFunction(second, first, pathResolver::isEpub)) return -1;
-        if (comparePathsFunction(first, second, pathResolver::isMobi)) return 1;
-        if (comparePathsFunction(second, first, pathResolver::isMobi)) return -1;
-        if (comparePathsFunction(first, second, pathResolver::isDocbook)) return 1;
-        if (comparePathsFunction(second, first, pathResolver::isDocbook)) return -1;
-        if (comparePathsFunction(first, second, pathResolver::isImage)) return 1;
-        if (comparePathsFunction(second, first, pathResolver::isImage)) return -1;
-        if (comparePathsFunction(first, second, Files::isDirectory)) return 1;
-        if (comparePathsFunction(second, first, Files::isDirectory)) return -1;
-
 
         if (OSHelper.isMac() || OSHelper.isWindows())
             return first.getFileName().toString().compareToIgnoreCase(second.getFileName().toString());
@@ -45,8 +22,4 @@ public class PathOrderService {
         return first.compareTo(second);
     }
 
-    private boolean comparePathsFunction(Path first, Path second, Function<Path, Boolean> function) {
-
-        return !function.apply(first) && function.apply(second);
-    }
 }
