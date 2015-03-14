5759670a3ef305ed206af1ccac772c454f148c0d
Tobias Roeser <le.petit.fou@web.de>
2015-03-14 15:53:09 +0100

    Removed unused import

--- src/main/java/com/kodcu/controller/AsciidocController.java
+++ src/main/java/com/kodcu/controller/AsciidocController.java
@@ -3,7 +3,6 @@ package com.kodcu.controller;
 import com.kodcu.other.Current;
 import com.kodcu.service.ThreadService;
 import com.kodcu.service.ui.TabService;
-import javafx.application.Platform;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
