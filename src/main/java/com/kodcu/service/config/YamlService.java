50aaddd78db3c707e39e2879ef2b2caeca0a7d45
Rahman Usta <rahman.usta.88@gmail.com>
2015-04-03 11:03:46 +0300

    Remove yamlbeans

--- src/main/java/com/kodcu/service/config/YamlService.java
+++ src/main/java/com/kodcu/service/config/YamlService.java
@@ -1,11 +1,11 @@
 package com.kodcu.service.config;
 
-import com.esotericsoftware.yamlbeans.YamlWriter;
 import com.kodcu.bean.Config;
 import com.kodcu.bean.RecentFiles;
 import com.kodcu.controller.ApplicationController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -28,17 +28,10 @@ public class YamlService {
     public void persist() throws IOException {
         Path configPath = controller.getConfigPath();
         File recentFileYml = configPath.resolve("recentFiles.yml").toFile();
-        YamlWriter yamlWriter = new YamlWriter(new FileWriter(recentFileYml));
-        yamlWriter.getConfig().setClassTag("RecentFiles", RecentFiles.class);
-        yamlWriter.write(new RecentFiles(controller.getRecentFiles()));
-        yamlWriter.close();
 
-        //
-
-        File configYml = configPath.resolve("config.yml").toFile();
-        yamlWriter = new YamlWriter(new FileWriter(configYml));
-        yamlWriter.getConfig().setClassTag("Config", Config.class);
-        yamlWriter.write(controller.getConfig());
-        yamlWriter.close();
+        Yaml yaml=new Yaml();
+        try(FileWriter writer = new FileWriter(recentFileYml);){
+            yaml.dump(new RecentFiles(controller.getRecentFiles()), writer);
+        }
     }
 }
