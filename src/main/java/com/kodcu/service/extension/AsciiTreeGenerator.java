5f6f610289f22213a33825d64f3dce6a4145bcc2
Hakan Özler <ozler.hakan@gmail.com>
2015-04-04 21:43:55 +0300

    Enhance ascii tree generator

--- src/main/java/com/kodcu/service/extension/AsciiTreeGenerator.java
+++ src/main/java/com/kodcu/service/extension/AsciiTreeGenerator.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Component;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * Created by usta on 16.03.2015.
@@ -17,45 +20,148 @@ public class AsciiTreeGenerator {
     }
 
     private String printDirectoryTree(Path folder) {
-
         int indent = 0;
+        Tree root = createInitialTree(folder, indent, new Tree());
+        root.setSiblingAndParent();
+
         StringBuilder sb = new StringBuilder();
-        printDirectoryTree(folder, indent, sb);
+        root.generateTree(sb);
+
         return sb.toString();
     }
 
-    private void printDirectoryTree(Path folder, int indent,
-                                    StringBuilder sb) {
-
-        sb.append(getIndentString(indent));
-        sb.append("|--");
-        sb.append(folder.getFileName().toString());
-        sb.append("\n");
+    private Tree createInitialTree(Path folder, int indent, Tree tree) {
+        tree.setDepth(indent);
+        tree.setName(folder.getFileName().toString());
 
         IOHelper.list(folder).forEach(p -> {
             if (IOHelper.isHidden(p))
                 return;
 
             if (Files.isDirectory(p))
-                printDirectoryTree(p, indent + 1, sb);
+                tree.getChildren().add(createInitialTree(p, indent + 1, new Tree()));
             else
-                printFile(p, indent + 1, sb);
+                createChild(p, indent + 1, tree);
         });
-
+        return tree;
     }
 
-    private void printFile(Path file, int indent, StringBuilder sb) {
-        sb.append(getIndentString(indent));
-        sb.append("|--");
-        sb.append(file.getFileName().toString());
-        sb.append("\n");
+    private void createChild(Path file, int indent, Tree parent) {
+        final Tree child = new Tree();
+        child.setName(file.getFileName().toString());
+        child.setDepth(indent);
+        parent.getChildren().add(child);
     }
 
-    private String getIndentString(int indent) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < indent; i++) {
-            sb.append("|  ");
+    private class Tree {
+
+        private int depth;
+        private String name;
+        private Tree parent;
+        private Tree sibling;
+        private ArrayList<Tree> children;
+
+        private Tree getNextSibling() {
+            return sibling;
         }
-        return sb.toString();
+
+        private void setNextSibling(Tree sibling) {
+            this.sibling = sibling;
+        }
+
+        private ArrayList<Tree> getChildren() {
+            if (Objects.isNull(children))
+                children = new ArrayList<>();
+
+            return children;
+        }
+
+        private void setChildren(ArrayList<Tree> children) {
+            this.children = children;
+        }
+
+        private int getDepth() {
+            return depth;
+        }
+
+        private void setDepth(int depth) {
+            this.depth = depth;
+        }
+
+        private String getName() {
+            return name;
+        }
+
+        private void setName(String name) {
+            this.name = name;
+        }
+
+        private Tree getParent() {
+            return parent;
+        }
+
+        private void setParent(Tree next) {
+            this.parent = next;
+        }
+
+        private void setSiblingAndParent() {
+            this.setSiblingAndParent(this);
+        }
+
+        private void setSiblingAndParent(Tree item) {
+            ArrayList<Tree> children = item.getChildren();
+            for (int index = 0; index < children.size(); index++) {
+                Tree currentChild = children.get(index);
+                currentChild.setParent(item);
+                if (index != children.size() - 1) {
+                    currentChild.setNextSibling(children.get(index + 1));
+                }
+                setSiblingAndParent(currentChild);
+            }
+        }
+
+        private void generateTree(StringBuilder sb) {
+            generateTree(this, sb);
+        }
+
+        private void generateTree(Tree tree, StringBuilder sb) {
+            printFile(sb, tree);
+
+            if (Objects.nonNull(tree.getChildren())) {
+                for (int index = 0; index < tree.getChildren().size(); index++) {
+                    generateTree(tree.getChildren().get(index), sb);
+                }
+            }
+        }
+
+        private void printFile(StringBuilder sb, Tree leaf) {
+            sb.append(getIndentString(leaf));
+
+            if (Objects.nonNull(leaf.getParent())) {
+                if (Objects.isNull(leaf.getNextSibling()))
+                    sb.append("`--");
+                else
+                    sb.append("|--");
+            }
+
+            sb.append(leaf.getName());
+            sb.append("\n");
+        }
+
+        private String getIndentString(Tree leaf) {
+            StringBuilder sb = new StringBuilder();
+            Optional<Tree> localTree = Optional.of(leaf);
+
+            for (int index = 1; index < leaf.getDepth(); index++) {
+                localTree = Optional.ofNullable(localTree.get().getParent());
+                localTree.ifPresent(p -> {
+                    if (Objects.nonNull(p.getNextSibling()))
+                        sb.append("  |");
+                    else
+                        sb.append("   ");
+                });
+            }
+            return sb.reverse().toString();
+        }
     }
 }
