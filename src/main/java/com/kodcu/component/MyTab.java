99dd2a21a7687bfa6acedc1f2ac072b17be88c24
Rahman Usta <rahman.usta.88@gmail.com>
2015-06-17 19:54:52 +0200

    Updates for markup language selector

--- src/main/java/com/kodcu/component/MyTab.java
+++ src/main/java/com/kodcu/component/MyTab.java
@@ -1,14 +1,12 @@
 package com.kodcu.component;
 
+import com.kodcu.other.DocumentMode;
 import com.kodcu.service.shortcut.AsciidocShortcutService;
 import com.kodcu.service.shortcut.MarkdownShortcutService;
 import javafx.application.Platform;
-import javafx.beans.property.ReadOnlyIntegerProperty;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
-import netscape.javascript.JSObject;
 
 import java.nio.file.Path;
 import java.util.Objects;
@@ -22,19 +20,6 @@ public class MyTab extends Tab {
 
     private EditorPane editorPane;
     private Path path;
-    private ChoiceBox<String> markup;
-
-
-    private void updateMarkup() {
-//        String tabText = getTabText();
-//        if (Objects.isNull(tabText) || Objects.isNull(markup))
-//            return;
-//
-//        if (tabText.contains(".md") || tabText.contains(".markdown"))
-//            markup.getSelectionModel().selectLast();
-//        else
-//            markup.getSelectionModel().selectFirst();
-    }
 
     public Label getLabel() {
         if (Objects.isNull(this.getGraphic()))
@@ -47,39 +32,17 @@ public class MyTab extends Tab {
         return label.getText();
     }
 
-    public boolean isAsciidoc() {
-        if (Objects.isNull(markup))
-            return true;
-        return markup.getSelectionModel().isSelected(0);
-    }
-
-    public boolean isMarkdown() {
-        if (Objects.isNull(markup))
-            return false;
-        return markup.getSelectionModel().isSelected(1);
-    }
-
     public void setTabText(String tabText) {
         Label label = getLabel();
         label.setText(tabText);
-        updateMarkup();
     }
-
 
     public Path getPath() {
         return path;
     }
 
     public void setPath(Path path) {
-        if (Objects.nonNull(markup)) {
-            markup.setManaged(Objects.isNull(path));
-            markup.setVisible(Objects.isNull(path));
-        }
         this.path = path;
-    }
-
-    public String htmlToMarkupFunction() {
-        return isAsciidoc() ? "toAsciidoc" : "toMarkdown";
     }
 
     public boolean isSaved() {
@@ -118,19 +81,6 @@ public class MyTab extends Tab {
         return true;
     }
 
-    private void cleanRemovedTabs() {
-
-        this.setPath(null);
-        this.setOnClosed(null);
-        this.setOnSelectionChanged(null);
-        this.setUserData(null);
-        this.getLabel().setOnMouseClicked(null);
-        this.setOnCloseRequest(null);
-        this.setEditorPane(null);
-        this.setGraphic(null);
-        this.setContent(null);
-    }
-
     public void select() {
         this.getTabPane().getSelectionModel().select(this);
     }
@@ -139,21 +89,6 @@ public class MyTab extends Tab {
         Platform.runLater(() -> {
             this.getTabPane().getTabs().remove(this); // keep it here
         });
-    }
-
-    public void setMarkup(ChoiceBox markup) {
-        this.markup = markup;
-        ReadOnlyIntegerProperty indexProperty = this.markup.getSelectionModel().selectedIndexProperty();
-        indexProperty.addListener((observable, oldValue, newValue) -> {
-            if ((oldValue != newValue) && Objects.nonNull(editorPane)) {
-                editorPane.switchMode(newValue);
-                editorPane.rerender(new Object[]{});
-            }
-        });
-    }
-
-    public ChoiceBox getMarkup() {
-        return markup;
     }
 
     @Override
@@ -167,6 +102,10 @@ public class MyTab extends Tab {
         return AsciidocShortcutService.class;
     }
 
+    public boolean isMarkdown() {
+        return editorPane.is("markdown");
+    }
+
     public void setEditorPane(EditorPane editorPane) {
         this.editorPane = editorPane;
     }
@@ -174,4 +113,13 @@ public class MyTab extends Tab {
     public EditorPane getEditorPane() {
         return editorPane;
     }
+
+    public boolean isAsciidoc() {
+        return editorPane.is("asciidoc");
+    }
+
+    public String htmlToMarkupFunction() {
+        return isAsciidoc() ? "toAsciidoc" : "toMarkdown";
+    }
+
 }
