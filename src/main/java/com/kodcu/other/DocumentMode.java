3bf0dfa560da3b579096d9e19d4439bce5c58696
Rahman Usta <rahman.usta.88@gmail.com>
2015-06-17 19:54:06 +0200

    Support for multiple files

---
+++ src/main/java/com/kodcu/other/DocumentMode.java
@@ -0,0 +1,53 @@
+package com.kodcu.other;
+
+/**
+ * Created by usta on 16.06.2015.
+ */
+public class DocumentMode {
+
+    private String caption;
+    private String extRe;
+    private String extensions;
+    private String mode;
+    private String name;
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public String getExtRe() {
+        return extRe;
+    }
+
+    public void setExtRe(String extRe) {
+        this.extRe = extRe;
+    }
+
+    public String getExtensions() {
+        return extensions;
+    }
+
+    public void setExtensions(String extensions) {
+        this.extensions = extensions;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
