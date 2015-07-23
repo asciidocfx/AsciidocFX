650a4a33f3cdaf60847a402953e2e32260387fc0
Russell Dickenson <russelldickenson@gmail.com>
2015-07-23 14:45:41 +1000

    Revert "Spelling suggestions 1"

--- src/main/java/com/kodcu/component/AlertHelper.java
+++ src/main/java/com/kodcu/component/AlertHelper.java
@@ -25,7 +25,7 @@ public final class AlertHelper {
 
     public static Optional<ButtonType> nullDirectoryAlert() {
         AlertDialog deleteAlert = new AlertDialog(AlertType.WARNING, null, ButtonType.OK);
-        deleteAlert.setHeaderText("Please select source and destination directories.");
+        deleteAlert.setHeaderText("Please select directorie(s)");
         return deleteAlert.showAndWait();
     }
 
