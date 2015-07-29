a261a85fd06e8bbc4806747f4e70649459d73568
Rahman Usta <rahman.usta.88@gmail.com>
2015-07-29 21:49:39 +0300

    Update slide templates

--- src/main/resources/public/js/asciidoctor-reveal.js
+++ src/main/resources/public/js/asciidoctor-reveal.js
@@ -22,65 +22,11 @@ var templateMap = {
 
                     var self = this;
 
+                    window.nodee = node;
+
                     var obj = {};
-                    obj.type = node.$type ? node.$type() : "";
+                    obj.node = node;
                     obj.name = node.$node_name ? node.$node_name() : "";
-                    obj.content = node.$content ? node.$content() : "";
-                    obj.text = node.$text ? node.$text() : "";
-                    obj.role = node.$role ? node.$role() : "";
-                    obj.option = node.$option ? node.$option() : "";
-                    obj.has_role = node.$has_role ? node.$has_role() : "";
-                    obj.style = node.$style ? node.$style() : "";
-                    obj.items = node.$items ? node.$items() : [];
-                    obj.title = node.$title ? node.$title() : null;
-                    obj.blocks = node.$blocks ? node.$blocks() : [];
-                    obj.sections = node.$sections ? node.$sections() : [];
-                    obj.level = node.$level ? node.$level() : null;
-                    obj.id = node.$id ? node.$id() : null;
-                    obj.columns = node.$columns ? node.$columns() : [];
-                    obj.rows = node.$rows ? node.$rows() : [];
-                    obj.captioned_title = node.$captioned_title ? node.$captioned_title() : null;
-                    obj.attr = {};
-
-                    obj.exist = function (e) {
-                        return (typeof e) !== "undefined";
-                    };
-
-                    obj.disregard = function (arr1, arr2) {
-                        var result = arr1.slice(0);
-                        for (var i = 0; i < result.length; i++) {
-                            var value = result[i];
-                            if (arr2.indexOf(value) != -1)
-                                result.splice(i, 1);
-                        }
-                        return result;
-                    };
-                    obj.self = self;
-                    obj.document = {
-                        attr: {}
-                    };
-
-                    var keys = node.$attributes().$keys();
-
-                    for (var i = 0; i < keys.length; i++) {
-                        var key = keys[i];
-                        var value = node.$attributes()['$[]'](key);
-                        obj.attr[key] = value;
-                    }
-
-
-                    obj.document.doctype = node.$document().$doctype();
-                    obj.document.doctitle = node.$document().$doctitle ? node.$document().$doctitle() : "";
-
-                    if (node.$document().$embedded)
-                        obj.document.embedded = node.$document().$embedded();
-
-                    var docKeys = node.$document().$attributes().$keys();
-                    for (var i = 0; i < docKeys.length; i++) {
-                        var key = docKeys[i];
-                        var value = node.$document().$attributes()['$[]'](key);
-                        obj.document.attr[key] = value;
-                    }
 
                     // Cache disabled icin comment yap
                     //var templateMap = {
