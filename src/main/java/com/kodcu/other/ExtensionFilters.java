3c595b207a1b30e9ae20c223fe313d30d0160050
Rahman Usta <rahman.usta.88@gmail.com>
2015-08-31 17:59:35 +0300

    Update Extension filters

--- src/main/java/com/kodcu/other/ExtensionFilters.java
+++ src/main/java/com/kodcu/other/ExtensionFilters.java
@@ -8,12 +8,14 @@ import javafx.stage.FileChooser;
 public class ExtensionFilters {
 
     public final static FileChooser.ExtensionFilter HTML = new FileChooser.ExtensionFilter("HTML", "*.html");
+    public final static FileChooser.ExtensionFilter XML = new FileChooser.ExtensionFilter("XML", "*.xml");
+    public final static FileChooser.ExtensionFilter ALL = new FileChooser.ExtensionFilter("All files", "*.*");
     public final static FileChooser.ExtensionFilter PDF = new FileChooser.ExtensionFilter("PDF", "*.pdf");
     public final static FileChooser.ExtensionFilter DOCBOOK = new FileChooser.ExtensionFilter("DOCBOOK", "*.xml");
     public final static FileChooser.ExtensionFilter EPUB = new FileChooser.ExtensionFilter("EPUB", "*.epub");
     public final static FileChooser.ExtensionFilter MOBI = new FileChooser.ExtensionFilter("MOBI", "*.mobi");
     public final static FileChooser.ExtensionFilter ODT = new FileChooser.ExtensionFilter("ODT", "*.odt");
-    public final static FileChooser.ExtensionFilter ASCIIDOC = new FileChooser.ExtensionFilter("AsciiDoc", "*.adoc", "*.asciidoc", "*.asc", "*.ad", "*.txt", "*.*");
-    public final static FileChooser.ExtensionFilter MARKDOWN = new FileChooser.ExtensionFilter("Markdown", "*.md", "*.markdown", "*.txt", "*.*");
+    public final static FileChooser.ExtensionFilter ASCIIDOC = new FileChooser.ExtensionFilter("AsciiDoc", "*.adoc", "*.asciidoc", "*.asc", "*.ad");
+    public final static FileChooser.ExtensionFilter MARKDOWN = new FileChooser.ExtensionFilter("Markdown", "*.md", "*.markdown");
 
 }
