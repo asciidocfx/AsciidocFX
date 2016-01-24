e1af179b445cd3a3c671c8e88d91622728cfc2d7
ozlerhakan <hakan.ozler@kodcu.com>
2016-01-24 23:56:37 +0200

    Add pdf converter service for cmd

---
+++ src/main/java/com/kodcu/commandline/service/CmdPdfConverter.java
@@ -0,0 +1,23 @@
+package com.kodcu.commandline.service;
+
+import org.asciidoctor.Asciidoctor;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
+import static org.asciidoctor.Asciidoctor.Factory.create;
+import static org.asciidoctor.OptionsBuilder.options;
+
+/**
+ * Created by Hakan on 1/22/2016.
+ */
+@Component
+public class CmdPdfConverter {
+
+    public void convert(String file) {
+        // just try asciidoctor-pdf service
+        Asciidoctor asciidoctor = create();
+        File inputFile = new File(file);
+        asciidoctor.convertFile(inputFile, options().backend("pdf").get());
+    }
+}
