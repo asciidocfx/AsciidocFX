ce24041c608d1178b08a41f8bcf8985330438b49
Rahman Usta <rahman.usta.88@gmail.com>
2016-05-26 22:18:56 +0300

    Fix pattern path issue with POST request

--- src/main/java/com/kodcu/controller/WebWorkerResource.java
+++ src/main/java/com/kodcu/controller/WebWorkerResource.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.nio.file.Path;
+import java.util.Optional;
 
 /**
  * Created by usta on 05.09.2015.
@@ -21,19 +22,27 @@ public class WebWorkerResource {
     private final FileService fileService;
     private final ThreadService threadService;
     private final ApplicationController controller;
+    private final DataUriController dataUriService;
 
     @Autowired
-    public WebWorkerResource(Current current, TabService tabService, DirectoryService directoryService, FileService fileService, ThreadService threadService, ApplicationController controller) {
+    public WebWorkerResource(Current current, TabService tabService, DirectoryService directoryService, FileService fileService, ThreadService threadService, ApplicationController controller, DataUriController dataUriService) {
         this.current = current;
         this.tabService = tabService;
         this.directoryService = directoryService;
         this.fileService = fileService;
         this.threadService = threadService;
         this.controller = controller;
+        this.dataUriService = dataUriService;
     }
 
 
     public void executeWorkerResource(AllController.Payload payload) {
+
+        Optional.ofNullable(payload.getRequestURI())
+                .filter(e -> e.endsWith("resource.afx"))
+                .ifPresent(e -> {
+                    payload.setFinalURI(payload.param("path"));
+                });
 
         String finalURI = payload.getFinalURI();
         if (finalURI.matches(".*\\.(asc|asciidoc|ad|adoc|md|markdown)$")) {
