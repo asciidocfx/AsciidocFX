15a055dc9ad5bf6d29b1f03eaec94d159fe83467
Rahman Usta <rahman.usta.88@gmail.com>
2016-08-31 23:46:51 +0300

    Change ui semaphore limit

--- src/main/java/com/kodcu/service/ThreadService.java
+++ src/main/java/com/kodcu/service/ThreadService.java
@@ -26,7 +26,7 @@ public class ThreadService {
         final int availableProcessors = Runtime.getRuntime().availableProcessors();
         final int corePoolSize = (availableProcessors * 2 >= 4) ? availableProcessors * 2 : 4;
         threadPollWorker = Executors.newScheduledThreadPool(corePoolSize);
-        uiSemaphore = new Semaphore(corePoolSize, true);
+        uiSemaphore = new Semaphore(250, true);
     }
 
     public ScheduledFuture<?> schedule(Runnable runnable, long delay, TimeUnit timeUnit) {
