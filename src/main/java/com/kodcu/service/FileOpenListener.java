413cd493ba5544f31d5161c9a5f4144ace3131a8
Rahman Usta <rahmanusta@kodedu.com>
2017-09-16 19:25:52 +0300

    File open matcher update

--- src/main/java/com/kodcu/service/FileOpenListener.java
+++ src/main/java/com/kodcu/service/FileOpenListener.java
@@ -1,12 +1,15 @@
 package com.kodcu.service;
 
 import com.install4j.api.launcher.StartupNotification;
+import com.kodcu.other.IOHelper;
 import com.kodcu.service.ui.TabService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
@@ -33,19 +36,34 @@ public class FileOpenListener implements StartupNotification.Listener {
         Pattern pattern = Pattern.compile("\"([^\"]+)\"|.*");
         Matcher matcher = pattern.matcher(parameters);
 
-        List<Path> pathSet = matcher.results()
-                .map(e -> e.group())
-                .map(e -> e.replaceAll("\"", ""))
-                .map(e -> e.trim())
-                .filter(e -> !e.isEmpty())
-                .map(Paths::get)
-                .map(Path::toAbsolutePath)
-                .distinct()
-                .collect(Collectors.toList());
+        List<Path> pathList = new ArrayList<>();
+
+        while (matcher.find()) {
+            String group = matcher.group();
+            group = group.replaceAll("\"", "");
+            group = group.trim();
+            if(!group.isEmpty()){
+                Path path = IOHelper.getPath(group).toAbsolutePath();
+                if (!pathList.contains(path)) {
+                    pathList.add(path);
+                }
+            }
+
+        }
+//
+//        List<Path> pathSet = matcher.results()
+//                .map(e -> e.group())
+//                .map(e -> e.replaceAll("\"", ""))
+//                .map(e -> e.trim())
+//                .filter(e -> !e.isEmpty())
+//                .map(Paths::get)
+//                .map(Path::toAbsolutePath)
+//                .distinct()
+//                .collect(Collectors.toList());
 
         threadService.runActionLater(() -> {
 
-            for (Path path : pathSet) {
+            for (Path path : pathList) {
                 tabService.addTab(path);
             }
         });
