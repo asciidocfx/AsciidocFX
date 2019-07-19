70f488b9f21c6178fcb1372d01457bb209d04e45
Vladimir Dvorak <lada.dvorak7@gmail.com>
2019-07-19 21:11:18 +0200

    Mark table cell by data-line

--- conf/public/js/asciidoctor-data-line.js
+++ conf/public/js/asciidoctor-data-line.js
@@ -9,12 +9,15 @@ Object.prototype._is_nil = function (name) {
 asciidoctor.Extensions.register(function () {
     this.treeProcessor(function () {
         const self = this;
+        const selector = JSON.stringify({
+        	traverse_documents: true
+        });
         self.process(function (parent, reader) {
 
             if (parent._is_nil("apply-data-line"))
                 return parent;
 
-            parent.$find_by().forEach(function (node) {
+            parent.$find_by(selector).forEach(function (node) {
                 if (node && node.$source_location) {
                     if (node.$source_location().lineno) {
                         var nodeName = node.node_name;
