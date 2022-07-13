92876c31148d982dee117431f3c26ab623bb8210
Rahman Usta <rahmanusta@kodedu.com>
2022-07-13 20:23:58 +0200

    Update ace

--- conf/public/ace/src/snippets/mips.js
+++ conf/public/ace/src/snippets/mips.js
@@ -1,5 +1,5 @@
-ace.define("ace/snippets/bro",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope=""});                (function() {
-                    ace.require(["ace/snippets/bro"], function(m) {
+;                (function() {
+                    ace.require(["ace/snippets/mips"], function(m) {
                         if (typeof module == "object" && typeof exports == "object" && module) {
                             module.exports = m;
                         }
