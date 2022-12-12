8c03ef7fcddd91aa56b16b3aed66d344cf4e15a1
Rahman Usta <rahmanusta@kodedu.com>
2022-12-12 20:18:25 +0100

    Buff helper improvements

--- conf/public/js/buffhelper.js
+++ conf/public/js/buffhelper.js
@@ -1,12 +1,41 @@
 function BufferedAction() {
 
-    var afterRun;
+    var afterRun1;
+    var afterRun2;
 
+    // Delays until all events stopped, then runs in timeout
     this.buff = function (code, delay) {
 
-        if (afterRun)
-            clearTimeout(afterRun);
+        if (afterRun1)
+            clearTimeout(afterRun1);
 
-        afterRun = setTimeout(code, delay);
+        afterRun1 = setTimeout(code, delay);
+    };
+
+    var codes = [];
+
+    // Runs one time in interval
+    this.throttle = function (code, delay) {
+        if (code) {
+            codes[0] = code;
+        }
+
+        if (afterRun2) {
+            return;
+        }
+
+        let popped = codes.pop();
+
+        if (!popped) {
+            return;
+        }
+
+        afterRun2 = setTimeout(() => {
+            popped();
+            afterRun2 = null;
+            if (codes.length > 0) {
+                this.throttle(null, delay);
+            }
+        }, delay);
     };
 }
